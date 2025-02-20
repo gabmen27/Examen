aacf5ec00297ba01c88ff34bb6b2d02b0492db7c
gabmen27 <gabrieljesus680@gmail.com>
2025-02-19 18:28:02 -0600

    Agregando los atributos de la calse Alumno

--- Examen/Alumno.cs
+++ Examen/Alumno.cs
@@ -8,9 +8,9 @@ namespace Examen
 {
     public class Alumno
     {
-        public string NombreAlumno { get; set; }
-        public string NumeroCuenta { get; set; }
-
+        public string? NombreAlumno { get; set; }
+        public string? NumeroCuenta { get; set; }
+        public string? Email { get; set; }
 
     }
 }
