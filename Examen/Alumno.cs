7cdb8ee4d93ba3d8d55eff1ce12d18977b65c361
gabmen27 <gabrieljesus680@gmail.com>
2025-02-19 18:26:45 -0600

    Created Alumno class

--- Examen/Alumno.cs
+++ Examen/Alumno.cs
@@ -8,9 +8,8 @@ namespace Examen
 {
     public class Alumno
     {
-        public string? NombreAlumno { get; set; }
-        public string? NumeroCuenta { get; set; }
-        public string? Email { get; set; }
+        public string NombreAlumno { get; set; }
+        public string NumeroCuenta { get; set; }
 
 
     }
